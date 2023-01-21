7cc84a3bc0c827d0991301e564848f8c085e4c53
Jeongseup <seup87@gmail.com>
2023-01-22 01:14:17 +0900

    update props

--- [udemy]best-react/my-app/src/components/ExpenseItem.js
+++ [udemy]best-react/my-app/src/components/ExpenseItem.js
@@ -1,13 +1,10 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  // const expenseDate = new Date(2023, 1, 22);
-  // const expenseTitle = 'Car Insurance';
-  // const expenseAmount = 200.14;
-
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
